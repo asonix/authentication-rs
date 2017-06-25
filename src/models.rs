bfe93ff15434554d6c96b78d1c0dcb1f0a084d0f
Riley Trautman <asonix.dev@gmail.com>
2017-06-25 17:32:43 -0500

    Get User from webtoken
    Get rid of more warnings

--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,7 @@
 use super::schema::users;
 use bcrypt::{DEFAULT_COST, hash, verify, BcryptResult};
 use frank_jwt::{Header, Payload, Algorithm, encode, decode};
+use diesel::prelude::*;
 
 #[derive(Queryable)]
 pub struct User {
@@ -38,13 +39,56 @@ impl User {
         }
     }
 
-    pub fn create_webtoken(&self) -> String {
+    pub fn create_webtoken(&self) -> Result<String, &'static str> {
+        if !self.verified {
+            return Err("User is not verified");
+        }
+
         let mut payload = Payload::new();
         payload.insert("id".to_string(), self.id.to_string());
         payload.insert("username".to_string(), self.username.clone());
         let header = Header::new(Algorithm::HS256);
         
-        encode(header, super::JWT_SECRET.to_string(), payload.clone())
+        Ok(encode(header,
+                  super::JWT_SECRET.to_string(),
+                  payload.clone()))
+    }
+
+    pub fn from_webtoken(webtoken: String) -> Result<Self, &'static str> {
+        use super::schema::users::dsl::*;
+
+        let db = match super::DB_POOL.get() {
+            Ok(conn) => super::DB(conn),
+            Err(_) => return Err("Could not get the database"),
+        };
+
+        let decoded = decode(webtoken,
+                             super::JWT_SECRET.to_string(),
+                             Algorithm::HS256);
+
+        let payload = match decoded {
+            Ok((_header, payload)) => payload,
+            Err(_) => return Err("Could not decode webtoken"),
+        };
+
+        let user_id = match payload.get("id") {
+            Some(user_id) => user_id,
+            None => return Err("Webtoken is invalid"),
+        };
+
+        let user_id: i32 = match user_id.parse::<i32>() {
+            Ok(user_id) => user_id,
+            Err(_) => return Err("Could not parse user_id from JWT"),
+        };
+
+        let user = users.filter(verified.eq(true))
+            .filter(id.eq(user_id))
+            .first::<Self>(db.conn());
+
+        match user {
+            Ok(user) => Ok(user),
+            Err(_) => Err("Could not fetch user from DB"),
+        }
     }
 }
 
